5179b79afd7fe64e6e79a15e412cf03163e118d5
Michael Magistro <mikemagistro@gmail.com>
2019-08-26 03:03:52 -0600

    homely debug: quick comment out

--- HOMELY.py
+++ HOMELY.py
@@ -4,4 +4,4 @@ from homely.files import symlink
 symlink('configs/.zshrc', '~/.zshrc')
 symlink('configs/.bashrc', '~/.bashrc')
 symlink('configs/sublime', '~/.config/sublime-text-3/Packages/User')
-symlink('configs/anki', '~/.local/share/Anki2/addons21')
+# symlink('configs/anki', '~/.local/share/Anki2/addons21')
