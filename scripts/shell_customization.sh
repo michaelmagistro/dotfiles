fadb7f2dcff2bcc8ae3ac9651b065eb32e50c3f7
Michael Magistro <mikemagistro@gmail.com>
2020-04-04 19:08:10 -0700

    rename and add git alias to .zshrc

---
+++ scripts/shell_customization.sh
@@ -0,0 +1,13 @@
+#!/bin/zsh
+#
+# Repo: michaelmagistro/dotfiles
+
+
+# SYMLINKS ##################################
+
+rm -f ~/.zshrc
+ln -s ~/projects/dotfiles/configs/.zshrc ~/.zshrc
+
+# SYMLINKS ##################################
+
+gpom="git push origin master"
--- scripts/symlinks.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/zsh
-#
-# Repo: michaelmagistro/dotfiles
-
-rm -f ~/.zshrc
-ln -s ~/projects/dotfiles/configs/.zshrc ~/.zshrc
